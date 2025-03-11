9700b849277dacb43542f24097b78323b77c1963
aokoye <aokoye@gmail.com>
2025-03-10 21:42:14 -0700

    Computer logic added

---
+++ javascript.js
@@ -0,0 +1,31 @@
+/* 
+Have the computer pick rock, paper, or scissors.
+Have the user pick rock, paper, or scissors.
+When the computer wins add one point to their score.
+when the human wins add one point to their score.
+If there is a tie no one gets a point.
+Have there be 5 rounds.
+---
+Rock beats scissors
+Paper beats rock
+Scissors beats paper
+*/
+
+function getComputerChoice() {
+    let ranChoice = Math.floor(Math.random() * 3);
+
+    let comChoice;
+
+    if (ranChoice == 0) {
+        comChoice = 'r';
+    } else if (ranChoice == 1) {
+        comChoice = 'p';
+    }
+    else {
+        comChoice = 's';
+    }
+
+    return comChoice;
+}
+
+console.log(getComputerChoice())
